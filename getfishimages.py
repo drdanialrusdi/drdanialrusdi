097e2e11e72323fb3158e393c9b1565b9512ba8e
drdanialrusdi <drdanialrusdi@yahoo.com>
2018-02-13 10:08:22 +0800

    Add files via upload

---
+++ getfishimages.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 11:54:32 2018
+
+@author: 00121074
+"""
+
+import scrapeimages
+
+sijil_nikah = ['sijil nikah malaysia', 'C:\\Users\\00130161\\Desktop\\Document classification and extraction\\data\\train\\sijil nikah']
+scrapeimages.main(sijil_nikah)
+#pelaling2 = ['ikan pelaling','D:/My Documents/2018/Pasar/Pelaling']
+#scrapeimages.main(pelaling2)
+#pelaling3 = ['indian mackerel','D:/My Documents/2018/Pasar/Pelaling']
+#scrapeimages.main(pelaling3)
+#pelaling4 = ['Rastrelliger kanagurta','D:/My Documents/2018/Pasar/Pelaling']
+#scrapeimages.main(pelaling4)
