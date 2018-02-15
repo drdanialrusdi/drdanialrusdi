bc25f762a6632b5832942738e2f894c6108da188
drdanialrusdi <drdanialrusdi@yahoo.com>
2018-02-15 10:11:54 +0800

    Update google_api_utils.py

--- google_api_utils.py
+++ google_api_utils.py
@@ -1,7 +1,6 @@
 import base64
-
 import requests
-
+import os
 
 class Service(object):
 
@@ -17,3 +16,31 @@ class Service(object):
 def encode_image(image):
     image_content = image.read()
     return base64.b64encode(image_content).decode()
+
+
+def main(photo_file):
+    """Run a text detection request on a single image"""
+
+    #access_token = os.environ.get('VISION_API')
+    access_token='AlzaSyClfmFYAi4u38L6FceacPQBbGsQS2UJril'
+    service = Service('vision', 'v1', access_token=access_token)
+
+    with open(photo_file, 'rb') as image:
+        base64_image = encode_image(image)
+        body = {
+            'requests': [{
+                'image': {
+                    'content': base64_image,
+                },
+                'features': [{
+                    'type': 'TEXT_DETECTION',
+                    'maxResults': 1,
+                }]
+
+            }]
+        }
+        response = service.execute(body=body)
+        text = response['responses'][0]['textAnnotations'][0]['description']
+        return response,text
+
+    
